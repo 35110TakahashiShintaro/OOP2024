a11f6f0aeeaf235bbcfba2177540fbd379787e33
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-04-18 13:37:12 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karinto = new Product(123,"かりんとう",180);
+
+            int price = karinto.Price;  //税抜きの金額
+
+            int taxIncluded = karinto.GetPriceIncludingTax();　//税込みの金額
+
+            Console.WriteLine(karinto.Name + "の税込み価格" + taxIncluded + "円【税抜き" + price + "円】");
+
+
+            Product daifuku = new Product(1234, "大福", 2000);
+
+            int price2 = daifuku.Price;  //税抜きの金額
+
+            int taxIncluded2 = daifuku.GetPriceIncludingTax();　//税込みの金額
+
+            Console.WriteLine(daifuku.Name + "の税込み価格" + taxIncluded2 + "円【税抜き" + price2 + "円】");
+        }
+    }
+}
---
+++ Chapter01/Samplep_38/Chapter01/Samplep_38/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samplep_38 {
+    internal class Student : Person {
+        public int ClassNumber { get; internal set; }
+        public int Grade { get; internal set; }
+    }
+}
