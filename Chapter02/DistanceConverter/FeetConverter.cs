6c5f2e7570f234200997dda840c02ab90591a519
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-04-18 15:01:11 +0900

    距離換算プログラム完成

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     public static class FeetConverter {
+
+       // private const double ratio = 0.3048; //定数
+        public static readonly double ratio = 0.3048;
+
         //メートルからフィートを求める
         public static double MeterToFeet(double meter) {
-            return meter / 0.3048;
+            return meter / ratio;
         }
         //フィートからメートルを求める
         public static double FeetToMeter(double feet) {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
