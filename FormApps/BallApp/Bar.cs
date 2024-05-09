8f21ca3f33765fed6d9a1cc232c25e5fd06935aa
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-09 15:59:55 +0900

    バー反射

--- FormApps/BallApp/Bar.cs
+++ FormApps/BallApp/Bar.cs
@@ -1,48 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BallApp {
-    internal class SoccerBall : Obj{
+    internal class Bar : Obj {
 
-        public static int Count { get; set; }
+        public Bar(double xp, double yp)
+            : base(xp, yp, @"Picture\bar.png") {
 
-        public SoccerBall(double xp, double yp)
-            : base(xp - 25, yp - 25, @"Picture\soccer_ball.png"){
+            MoveX = 10;
+            MoveY = 0;
 
-            var rand2 = new Random();
- 
-
-            MoveX = rand2.Next(-50, 50); //移動量設定
-            MoveY = rand2.Next(-50, 50);
-
-            Count++;
         }
-
         public override bool Move(PictureBox pbBar, PictureBox pbBall) {
-
             Rectangle rBar = new Rectangle(pbBar.Location.X, pbBar.Location.Y,
                                                      pbBar.Width, pbBar.Height);
 
             Rectangle rBall = new Rectangle(pbBall.Location.X, pbBall.Location.Y,
                                                      pbBall.Width, pbBall.Height);
 
-            if (PosX > 750 || PosX < 0) {
-                MoveX = -MoveX;
-            }
-            if (PosY > 500 || PosY < 0 || rBar.IntersectsWith(rBall)) {
-                MoveY = -MoveY;
-            }
-            PosX += MoveX; 
-            PosY += MoveY;
-
             return true;
         }
 
         public override bool Move(Keys direction) {
+            if(direction == Keys.Right) {
+                if(PosX < 630) {
+                    PosX += MoveX;
+                }
+            } else if(direction == Keys.Left) { 
+                if(PosX > 0) {
+                    PosX -= MoveX;
+                }
+            }
             return true;
         }
     }
--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -10,6 +10,10 @@ namespace BallApp {
         private List<Obj> balls = new List<Obj>(); //ボールインスタンス格納用
         private List<PictureBox> pbs = new List<PictureBox>(); //表示用 
 
+        //バー用
+        private Bar bar;
+        private PictureBox pbBar;
+
         //コンストラクタ
         public Form1() {
             InitializeComponent();
@@ -17,18 +21,28 @@ namespace BallApp {
 
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
-            this.Text = "BallApp SoccerBall:" + balls.Count + "TennisBall:" + balls.Count;
+            this.Text = "BallApp SoccerBall:0  TennisBall:0";
+
+            bar = new Bar(340, 500);
+            pbBar = new PictureBox();
+            pbBar.Image = bar.Image;
+            pbBar.Location = new Point((int)bar.PosX, (int)bar.PosY);
+            pbBar.Size = new Size(150, 10);
+            pbBar.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbBar.Parent = this;
+
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-            
-            for(int i = 0; i < balls.Count; i++) {
-                balls[i].Move();
+
+            for (int i = 0; i < balls.Count; i++) {
+                balls[i].Move(pbBar, pbs[i]);
                 pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
             }
 
         }
 
+        //マウスクリックイベントハンドラ
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
             PictureBox pb = new PictureBox();  //画像を表示するコントロール
@@ -37,7 +51,7 @@ namespace BallApp {
             if (e.Button == MouseButtons.Left) {
                 ball = new SoccerBall(e.X - 25, e.Y - 25);
                 pb.Size = new Size(50, 50);
-            }else if (e.Button == MouseButtons.Right) {
+            } else if (e.Button == MouseButtons.Right) {
                 ball = new TennisBall(e.X - 12, e.Y - 12);
                 pb.Size = new Size(25, 25);
             }
@@ -47,11 +61,18 @@ namespace BallApp {
             pb.Parent = this;
             timer1.Start();
 
-            balls.Count();
+            
             balls.Add(ball);
             pbs.Add(pb);
 
             this.Text = "BallApp SoccerBall:" + SoccerBall.Count + "TennisBall:" + TennisBall.Count;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+
+            bar.Move(e.KeyCode);
+            pbBar.Location = new Point((int)bar.PosX, (int)bar.PosY);
 
         }
     }
