6efbd9c6be2a0efeb04749f4d79c599b8edbe447
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-15 14:10:25 +0900

    スペースを大文字

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,42 +7,45 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 2, 6, 8, 4, 3, 2, 4, 2, 2, 2, };
+            var names = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            //int count = numbers.Count(n => n % 2 == 0);
-            double num = numbers.Where(n => 5 < n).Average();
-            int total = numbers.Where(n => 5 < n).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(total);
+            IEnumerable<string> query = names.Where(s => s.Contains(" "))
+                                             .Select(s => s.ToUpper());
+            foreach(string s in query) {
+                Console.WriteLine(s);
+            }
+
+
+
+
+
+            //var lowerList = list.ConvertAll(s  => s.ToLower());
+            //lowerList.ForEach(s => Console.WriteLine(s));
+
+            //var upperList = list.ConvertAll(h => h.ToUpper());
+            //lowerList.ForEach(h => Console.WriteLine(h));
+
+            //var removeCount = list.RemoveAll(s => s.Contains("on"));
+            //Console.WriteLine(removeCount);
+
+            //list.ForEach(s => Console.WriteLine(s));
+
+            //foreach( var item in list) {
+            //    Console.WriteLine(item);
+            //}
+            //var names = list.Where(s => s.Length <= 9);
+            //foreach ( var s in names)
+            //    Console.WriteLine(s);
         }
 
-
-
-
-
-        ////nが偶数かどうかを調べる
-        //public static bool IsEven(int n) {
-        //    return n % 2 == 0; //偶数だとtrueが返される
-        //}
-
-        ////nが奇数かどうか調べる
-        //public static bool IsNotEven(int n) {
-        //    return n % 2 == 1;
-        //}
-
-        ////7より大きい数値があるかどうか調べる
-        //public static bool IsNumOver(int n) {
-        //    return 5 < n;
-        //}
-
-        ////public delegate bool Judgement(int value); //デリゲートの宣言
-        //public static int Count(int[]numbers, Predicate<int> judge) {
-        //    int count = 0;
-        //    foreach (var n in numbers) {
-        //        if(judge(n) == true) 
-        //            count++;
-        //    }
-        //    return count;
-        //}
     }
 }
