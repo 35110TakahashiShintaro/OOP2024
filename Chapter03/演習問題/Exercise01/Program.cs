f3c8a2011a8f561cf2bccf27335e3cf7faae4409
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-15 15:08:20 +0900

    Exercise1_2

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -31,15 +31,16 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(List<int> numbers) {
-
+            numbers.ForEach(n => Console.WriteLine(n / 2));            
         }
 
         private static void Exercise1_3(List<int> numbers) {
-
+            numbers.Where(n => 50 <= n);
         }
 
         private static void Exercise1_4(List<int> numbers) {
-
+            var bi = numbers.Select(n => n * 2);
+            Console.WriteLine(numbers);
         }
     }
 }
