25d81191b2febaed9c8ba7b1d33633f8e3a6a1b7
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-29 15:39:33 +0900

    5-1

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+
+            if (String.Compare(s1, s2,true) == 0 ){
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+            
+        }
+    }
+}
