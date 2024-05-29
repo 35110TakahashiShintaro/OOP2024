462c5ad1d990fc666818cf5ec057bdf1cd76fe68
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-29 11:44:18 +0900

    4.1終わり

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+        //4.1.1
+        public int Year {  get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //4.1.2
+        public bool Is21Century {
+            get {
+                return  2001 <= Year && Year <= 2100;
+            }
+        }
+
+        //4.1.3
+        public YearMonth AddOneMounth() { 
+            int newYear = Year;
+            int newMonth = Month + 1;
+
+            if (newMonth > 12) {
+                newMonth = 1;
+                newYear++;
+            }
+
+            return new YearMonth(newYear, newMonth);
+        }
+
+        //4.1.4
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
