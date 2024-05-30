aa0b92cb203ae46c784bdef9f8c0aa8fe9e2684c
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-30 15:47:43 +0900

    3-6

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -8,6 +8,8 @@ namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
+            var text2 = "Jackdaws,love my,big sphinx-of_quartz";
+
 
             Exercise3_1(text);
             Console.WriteLine("-----");
@@ -22,28 +24,30 @@ namespace Exercise03 {
             Console.WriteLine("-----");
 
             Exercise3_5(text);
-        }
+
+            Exercise3_6(text2);
+
+        }               
 
         private static void Exercise3_1(string text) {
-            int SpaceCount = text.Count(c => c == ' ');
-            Console.WriteLine("空白の数: " + SpaceCount);
+            int kara = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0} " + kara);
         }
 
         private static void Exercise3_2(string text) {
 
-            string ok = text.Replace("big", "small");
-            Console.WriteLine("置き換え後の文字列: " + ok);
+            string replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-            var str = text.Split(' ').Length;
-            Console.WriteLine(str);
-
+            var tango = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}" + tango);
         }
 
         private static void Exercise3_4(string text) {
-            var str = text.Split(' ').Where(word => word.Length <= 4);
-            foreach (var word in str) {
+            var words = text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var word in words) {
                 Console.WriteLine(word);
             }
         }
@@ -52,9 +56,18 @@ namespace Exercise03 {
             var str = text.Split(' ').ToArray();
             StringBuilder ss = new StringBuilder();
             foreach (string word in str) {
-                ss.Append(word).Append(' ');
+                ss.Append(word);
+                ss.Append(' ');
             }
             Console.WriteLine(ss);
+        }
+
+        private static void Exercise3_6(string text2) {
+            var kigo = text2.Split(',','-','_',' ').Where(s => s.Length <= 4);            
+
+            foreach (var word in kigo) {
+                Console.WriteLine(word);
+            }            
         }
     }
 }
