45b7bf8646ba0013fea1b0febf9a9a1c40e209f3
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-05-30 14:56:12 +0900

    5-3

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+
+            string st = Console.ReadLine();
+            int num;
+
+            string Comma;
+
+            if (int.TryParse(st, out num)) {
+                Comma = num.ToString("#,0");
+                Console.WriteLine(Comma);
+            }
         }
     }
 }
--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -25,23 +25,36 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_1(string text) {
-        
+            int SpaceCount = text.Count(c => c == ' ');
+            Console.WriteLine("空白の数: " + SpaceCount);
         }
 
         private static void Exercise3_2(string text) {
-        
+
+            string ok = text.Replace("big", "small");
+            Console.WriteLine("置き換え後の文字列: " + ok);
         }
 
         private static void Exercise3_3(string text) {
-        
+            var str = text.Split(' ').Length;
+            Console.WriteLine(str);
+
         }
 
         private static void Exercise3_4(string text) {
-        
+            var str = text.Split(' ').Where(word => word.Length <= 4);
+            foreach (var word in str) {
+                Console.WriteLine(word);
+            }
         }
 
         private static void Exercise3_5(string text) {
-        
+            var str = text.Split(' ').ToArray();
+            StringBuilder ss = new StringBuilder();
+            foreach (string word in str) {
+                ss.Append(word).Append(' ');
+            }
+            Console.WriteLine(ss);
         }
     }
 }
