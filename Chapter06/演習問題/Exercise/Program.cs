09514d232ca852b195edb29e25f7af6caafa53f3
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-06-05 13:41:27 +0900

    1-1~1-4まで完成

--- Chapter06/演習問題/Exercise/Program.cs
+++ Chapter06/演習問題/Exercise/Program.cs
@@ -24,25 +24,36 @@ namespace Exercise01 {
             Exercise1_5(numbers);
         }
 
-        private static void Exercise1_1(int[] numbers) {
-            int max = Max(numbers);
-            Console.WriteLine(numbers);
-        }
-
-        private static int Max(int[] numbers) {
-
+        private static void Exercise1_1(int[] numbers) {            
+            Console.WriteLine(numbers.Max());
         }
 
         private static void Exercise1_2(int[] numbers) {
+            Console.WriteLine(numbers.Reverse().Skip(1).First());
         }
-
+                
         private static void Exercise1_3(int[] numbers) {
+            foreach(int num in numbers) {
+                Console.WriteLine(num.ToString());
+            }
         }
 
         private static void Exercise1_4(int[] numbers) {
+            var Numbers = numbers.OrderBy(x => x).Take(3);
+            foreach (int num in Numbers) {
+                Console.WriteLine(num);
+            }
         }
 
         private static void Exercise1_5(int[] numbers) {
+            var zyufuku = numbers.Distinct();
+            foreach (int num in zyufuku) {
+                if(10 < num) {
+                    Console.WriteLine(num);
+                } else {
+
+                }
+            }
         }
     }
 }
