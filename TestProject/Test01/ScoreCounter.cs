fd3f63efe5f2b84b5a3f1c3c09509a4c986efd62
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-06-18 11:07:45 +0900

    設問1_Studentクラス完成

---
+++ TestProject/Test01/ScoreCounter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Test01 {
+    class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+        }
+
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                students.Add(student);
+            }
+            return students;
+
+
+
+
+        }
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var student in _students) {
+                if (dict.ContainsKey(student.Name)) {
+                    dict[student.ShopName] += student.Amount;
+                } else {
+                    dict[student.ShopName] = student.Amount;
+                }
+            }
+            return dict;
+
+
+
+
+        }
+    }
+}
