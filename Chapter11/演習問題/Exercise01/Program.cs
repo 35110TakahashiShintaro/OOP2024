83359f9d84df1d096dae710c329eca6a2d48570f
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-07-17 15:28:14 +0900

    競技名とチームメンバー数

--- Chapter11/演習問題/Exercise01/Program.cs
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var teams = xdoc.Root.Elements().Select(x => new {
+                Name = (string)x.Element("name"),
+                Member = (int)x.Element("teammembers")
+            });
+
+            foreach (var team in teams) {
+                Console.WriteLine($"競技名: {team.Name}, メンバー数: {team.Member}");
+            }
+
+        }
+
+        private static void Exercise1_2(string file) {
+
+        }
+
+        private static void Exercise1_3(string file) {
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+
         }
     }
 }
