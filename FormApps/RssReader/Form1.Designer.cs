c7279e524211698a13912a3cb7b5603e1de1ef1e
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-09-11 14:52:28 +0900

    P327まで完了（Modelの作成）

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -26,26 +26,29 @@
             this.tbRssUrl = new System.Windows.Forms.TextBox();
             this.btGet = new System.Windows.Forms.Button();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
-            this.webView1 = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
-            ((System.ComponentModel.ISupportInitialize)(this.webView1)).BeginInit();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.tbRssSearch = new System.Windows.Forms.TextBox();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.btOkini = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
             // tbRssUrl
             // 
-            this.tbRssUrl.Location = new System.Drawing.Point(26, 13);
+            this.tbRssUrl.Location = new System.Drawing.Point(26, 12);
             this.tbRssUrl.Name = "tbRssUrl";
-            this.tbRssUrl.Size = new System.Drawing.Size(643, 19);
+            this.tbRssUrl.Size = new System.Drawing.Size(884, 19);
             this.tbRssUrl.TabIndex = 0;
             // 
             // btGet
             // 
-            this.btGet.Location = new System.Drawing.Point(675, 12);
+            this.btGet.Location = new System.Drawing.Point(916, 10);
             this.btGet.Name = "btGet";
-            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.Size = new System.Drawing.Size(130, 23);
             this.btGet.TabIndex = 1;
-            this.btGet.Text = "取得";
+            this.btGet.Text = "URL検索";
             this.btGet.UseVisualStyleBackColor = true;
-            this.btGet.Click += new System.EventHandler(this.btGet_Click);
             // 
             // lbRssTitle
             // 
@@ -57,26 +60,71 @@
             this.lbRssTitle.TabIndex = 2;
             this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
             // 
-            // webView1
+            // webView21
             // 
-            this.webView1.Location = new System.Drawing.Point(26, 274);
-            this.webView1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webView1.Name = "webView1";
-            this.webView1.Size = new System.Drawing.Size(1020, 420);
-            this.webView1.TabIndex = 4;
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(26, 274);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1020, 417);
+            this.webView21.TabIndex = 5;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // tbRssSearch
+            // 
+            this.tbRssSearch.Location = new System.Drawing.Point(26, 68);
+            this.tbRssSearch.Name = "tbRssSearch";
+            this.tbRssSearch.Size = new System.Drawing.Size(884, 19);
+            this.tbRssSearch.TabIndex = 6;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(916, 68);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(130, 19);
+            this.btSearch.TabIndex = 7;
+            this.btSearch.Text = "キーワード検索";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(26, 42);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(884, 20);
+            this.cbCategory.TabIndex = 8;
+            this.cbCategory.SelectedIndexChanged += new System.EventHandler(this.cbCategory_SelectedIndexChanged);
+            // 
+            // btOkini
+            // 
+            this.btOkini.Location = new System.Drawing.Point(916, 39);
+            this.btOkini.Name = "btOkini";
+            this.btOkini.Size = new System.Drawing.Size(130, 23);
+            this.btOkini.TabIndex = 9;
+            this.btOkini.Text = "お気に入り登録";
+            this.btOkini.UseVisualStyleBackColor = true;
+            this.btOkini.Click += new System.EventHandler(this.btOkini_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1072, 757);
-            this.Controls.Add(this.webView1);
+            this.Controls.Add(this.btOkini);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.btSearch);
+            this.Controls.Add(this.tbRssSearch);
+            this.Controls.Add(this.webView21);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.btGet);
             this.Controls.Add(this.tbRssUrl);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.webView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,7 +135,11 @@
         private System.Windows.Forms.TextBox tbRssUrl;
         private System.Windows.Forms.Button btGet;
         private System.Windows.Forms.ListBox lbRssTitle;
-        private Microsoft.Toolkit.Forms.UI.Controls.WebView webView1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.TextBox tbRssSearch;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.Button btOkini;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,45 +1,171 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows.Forms;
 using System.Xml.Linq;
-using Windows.UI.Xaml.Controls.Primitives;
-using static System.Windows.Forms.LinkLabel;
 
 namespace RssReader {
-
     public partial class Form1 : Form {
         List<ItemDate> items;
+        private List<ItemDate> filteredItems;
+        List<KeyValuePair<string, string>> favoriteCategories = new List<KeyValuePair<string, string>>();
 
         public Form1() {
             InitializeComponent();
+            InitializeWebView2Async();
+            LoadFavorites();
+            UpdateCategoryList();
+            this.Resize += new EventHandler(this.Form_Resize);
+            btGet.Click += new EventHandler(this.btGet_Click);
+            lbRssTitle.SelectedIndexChanged += new EventHandler(this.lbRssTitle_SelectedIndexChanged);
+            tbRssUrl.TextChanged += new EventHandler(this.lbRssTitle_SelectedIndexChanged);
+            btSearch.Click += new EventHandler(this.btSearch_Click);
+            cbCategory.SelectedIndexChanged += new EventHandler(this.cbCategory_SelectedIndexChanged);
+
+            
         }
 
-        private void btGet_Click(object sender, EventArgs e) {
-            using (var wc = new WebClient()) {
-                var url = wc.OpenRead(tbRssUrl.Text);
-                var xdoc = XDocument.Load(url);
+        private void UpdateCategoryList() {
+            cbCategory.Items.Clear();
 
-                items = xdoc.Descendants("item")
-                                 .Select(item => new ItemDate{
-                                     Title = item.Element("title").Value,
-                                     Link = item.Element("link").Value,
-                                 }).ToList();
+            cbCategory.DisplayMember = "Key";
 
-                foreach (var item in items) {
-                    lbRssTitle.Items.Add(item.Title); 
+            cbCategory.Items.Add(new KeyValuePair<string, string>("主要", "https://news.yahoo.co.jp/rss/topics/top-picks.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("国内", "https://news.yahoo.co.jp/rss/topics/domestic.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("国際", "https://news.yahoo.co.jp/rss/topics/world.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("経済", "https://news.yahoo.co.jp/rss/topics/business.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("エンタメ", "https://news.yahoo.co.jp/rss/topics/entertainment.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("スポーツ", "https://news.yahoo.co.jp/rss/topics/sports.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("IT", "https://news.yahoo.co.jp/rss/topics/it.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("科学", "https://news.yahoo.co.jp/rss/topics/science.xml"));
+            cbCategory.Items.Add(new KeyValuePair<string, string>("地域", "https://news.yahoo.co.jp/rss/topics/local.xml"));
+
+            if (favoriteCategories.Any()) {
+                cbCategory.Items.Add(new KeyValuePair<string, string>("--- お気に入り ---", "")); // 区切り
+                foreach (var favorite in favoriteCategories) {
+                    cbCategory.Items.Add(favorite);
                 }
+            }
+
+            // 初期選択
+            if (cbCategory.Items.Count > 0) {
+                cbCategory.SelectedIndex = 0;
             }
         }
 
+        private async void InitializeWebView2Async() {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+
+
+        private void btGet_Click(object sender, EventArgs e) {
+            try {
+                using (var wc = new WebClient()) {
+
+                    var url = wc.OpenRead(tbRssUrl.Text);
+                    var xdoc = XDocument.Load(url);
+                    items = xdoc.Descendants("item")
+                                 .Select(item => new ItemDate {
+                                     Title = item.Element("title").Value,
+                                     Link = item.Element("link").Value,
+                                 }).ToList();
+                    //DisplayItems(items);
+                    lbRssTitle.Items.Clear();
+                    foreach (var item in items) {
+                        lbRssTitle.Items.Add(item.Title);
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"RSS フィードの読み込み中にエラーが発生しました: {ex.Message}");
+            }
+        }
+
+
+
+
+        private void Form_Resize(object sender, EventArgs e) {
+            // サイズ変更時の処理をここに追加
+        }
+
+        private void btSearch_Click(object sender, EventArgs e) {
+            if (items == null || !items.Any()) return;
+            var searchText = tbRssSearch.Text.ToLower();
+            filteredItems = items
+                .Where(item => item.Title.ToLower().Contains(searchText))
+                .ToList();
+            DisplayItems(filteredItems);
+            if (!filteredItems.Any()) {
+                lbRssTitle.Items.Clear();
+                lbRssTitle.Items.Add("該当する記事がありません");
+            }
+        }
         private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
-            webView1.Navigate(items[lbRssTitle.SelectedIndex].Link);
+            if (lbRssTitle.SelectedIndex >= 0) {
+                var selectedIndex = lbRssTitle.SelectedIndex;
+
+                // filteredItems がnullまたは空の場合、itemsを使用する
+                var itemList = (filteredItems != null && filteredItems.Any()) ? filteredItems : items;
+
+                if (itemList != null && selectedIndex >= 0 && selectedIndex < itemList.Count) {
+                    var selectedItem = itemList[selectedIndex];
+                    webView21.CoreWebView2.Navigate(selectedItem.Link);
+                }
+            }
+
+        }
+        private void DisplayItems(List<ItemDate> itemsToDisplay) {
+            lbRssTitle.Items.Clear();
+            if (itemsToDisplay == null || itemsToDisplay.Count == 0) {
+                lbRssTitle.Items.Add("該当する記事がありません");
+                return;
+            }
+            foreach (var item in itemsToDisplay) {
+                lbRssTitle.Items.Add(item.Title);
+            }
+        }
+
+        private void cbCategory_SelectedIndexChanged(object sender, EventArgs e) {
+            if (cbCategory.SelectedItem is KeyValuePair<string, string> selectedCategory) {
+                tbRssUrl.Text = selectedCategory.Value; // URLをテキストボックスにセットする
+                btGet.PerformClick(); // "取得"ボタンをプログラムからクリックする
+            }
+        }
+
+        private void btOkini_Click(object sender, EventArgs e) {
+            if (cbCategory.SelectedItem is KeyValuePair<string, string> selectedCategory &&
+                !favoriteCategories.Any(c => c.Key == selectedCategory.Key)) {
+                favoriteCategories.Add(selectedCategory);
+                SaveFavorites(); // お気に入りを保存
+                UpdateCategoryList();
+                MessageBox.Show($"{selectedCategory.Key} がお気に入りに追加されました！");
+            } else {
+                MessageBox.Show("すでにお気に入りに登録されています。");
+            }
+        }
+        private void SaveFavorites() {
+            var favoriteData = favoriteCategories.Select(c => $"{c.Key},{c.Value}").ToList();
+            System.IO.File.WriteAllLines("favorites.txt", favoriteData);
+        }
+
+        private void LoadFavorites() {
+            if (System.IO.File.Exists("favorites.txt")) {
+                favoriteCategories = System.IO.File.ReadAllLines("favorites.txt")
+                    .Select(line => line.Split(','))
+                    .Select(parts => new KeyValuePair<string, string>(parts[0], parts[1]))
+                    .ToList();
+            }
         }
     }
+
     public class ItemDate {
         public string Title { get; set; }
         public string Link { get; set; }
     }
+
+
+    
 }
+
