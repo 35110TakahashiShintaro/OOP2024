26ffc7c4edf6284e1e240b8238171bb782e90699
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-09-30 14:44:58 +0900

    1-2,1-3

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var groops = Library.Books.GroupBy(b => b.PublishedYear)
+                                      .OrderBy(g => g.Key);
+            
+            foreach (var book in groops) {
+                Console.WriteLine($"{book.Key}年");
+                foreach (var books in book) {
+                    Console.WriteLine($"{books}");
+                }
+            }
+            
+
+
+        }
+    }
+}
---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,61 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var max = Library.Books
+                               .Max(x => x.Price);
+            var book = Library.Books
+                                  .First(c => c.Price == max);
+            Console.WriteLine(book);
+        }
+
+        private static void Exercise1_3() {
+            var booksYear = Library.Books
+                                   .GroupBy(b => b.PublishedYear)
+                                   .Select(g => new { Year = g.Key, Count = g.Count() });
+
+            foreach (var item in booksYear) {
+                Console.WriteLine($"{item.Year}年: {item.Count}冊");
+            }
+        }
+
+        private static void Exercise1_4() {
+        }
+
+        private static void Exercise1_5() {
+        }
+
+        private static void Exercise1_6() {
+        }
+
+        private static void Exercise1_7() {
+        }
+
+        private static void Exercise1_8() {
+        }
+    }
+}
