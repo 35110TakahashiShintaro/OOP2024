862de088788dcad3dd0b31e8a5ad5dc2938d43ed
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-10-10 10:32:12 +0900

    17-2

--- Chapter17/Strategy/DistanceConverter/ConverterFactory.cs
+++ Chapter17/Strategy/DistanceConverter/ConverterFactory.cs
@@ -13,6 +13,8 @@ namespace DistanceConverter {
             new FeetConverter(),
             new YardConverter(),
             new InchConverter(),
+            new MileConverter(),
+            new KiroConverter(),
         };
 
         public static ConverterBase GetInstance(string name) {
