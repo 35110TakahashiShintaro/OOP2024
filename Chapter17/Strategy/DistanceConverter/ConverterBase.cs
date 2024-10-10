1582330e6786aeff06b058e50800c50ca2426b79
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-10-10 10:17:13 +0900

    P429まで終わり

---
+++ Chapter17/Strategy/DistanceConverter/ConverterBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public abstract class ConverterBase {
+
+        public abstract bool IsMyUnit(string name);
+
+        // メートルとの比率（この比率をかけるとメートルに変換できる）
+        protected abstract double Ratio { get; }
+
+        // 距離の単位名（たとえば、"メートル"、"フィート"など）
+        public abstract string UnitName { get; }
+
+        // メートルからの変換
+        public double FromMeter(double meter) {
+            return meter / Ratio;
+        }
+
+        // メートルへの変換
+        public double ToMeter(double value) {
+            return value * Ratio;
+        }
+
+
+    }
+}
