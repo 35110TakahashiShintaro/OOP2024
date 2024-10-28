ac969bd0bcb2def962e756eb9b81644bc3088e63
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-10-28 13:47:29 +0900

    コンボボックスで色選択、保存

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -10,22 +11,29 @@ namespace CollorChecker {
 
         public MainWindow() {
             InitializeComponent();
-
+            currentColor = new MyColor();
+            DataContext = GetColorList();
             currentColor.Color = Color.FromArgb(255, 0, 0, 0);
         }
 
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+        .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name })
+        .ToArray();
+        }
+
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            currentColor.Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value); colorArea.Background = new SolidColorBrush(currentColor.Color);
+            currentColor.Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value);
             colorArea.Background = new SolidColorBrush(currentColor.Color);
-            
+
         }
 
         private void StockButton_Click(object sender, RoutedEventArgs e) {
-            if (LB.Items.OfType<MyColor>().Any(c => c.Color.Equals(currentColor.Color))) {
-                MessageBox.Show("この色はすでに保存されています。");
-                return;
+            if (!LB.Items.OfType<MyColor>().Any(color => color.Color == currentColor.Color)) {
+                LB.Items.Insert(0, currentColor);
+            } else {
+                MessageBox.Show("この色は追加済です。");
             }
-            LB.Items.Insert(0, new MyColor { Color = currentColor.Color });
         }
 
         private void LB_SelectionChanged(object sender, SelectionChangedEventArgs e) {
@@ -35,6 +43,19 @@ namespace CollorChecker {
             bSlider.Value = ((MyColor)LB.Items[LB.SelectedIndex]).Color.B;
 
             
+        }
+
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (colorSelectComboBox.SelectedItem != null) {
+                MyColor selectedColor = (MyColor)colorSelectComboBox.SelectedItem;
+
+                colorArea.Background = new SolidColorBrush(selectedColor.Color);
+
+                rSlider.Value = selectedColor.Color.R;
+                gSlider.Value = selectedColor.Color.G;
+                bSlider.Value = selectedColor.Color.B;
+            }
+
         }
     }
 }
