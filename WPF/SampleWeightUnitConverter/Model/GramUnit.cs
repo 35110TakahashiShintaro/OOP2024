8700f91efd41abf9f61819e75bc404d6648880fa
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-11-07 11:15:54 +0900

    単位変換グラム、ポンド

---
+++ WPF/SampleWeightUnitConverter/Model/GramUnit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter {
+    internal class GramUnit : WeightUnit {
+        private static readonly List<GramUnit> units = new List<GramUnit> {
+            new GramUnit { Name = "mg", Coefficient = 0.001 },    // ミリグラム
+            new GramUnit { Name = "g", Coefficient = 1 },         // グラム
+            new GramUnit { Name = "kg", Coefficient = 1000 },     // キログラム
+            new GramUnit { Name = "ct", Coefficient = 0.2 },      // カラット 
+            
+        };
+
+        public static ICollection<GramUnit> Units { get { return units; } }
+
+        public double FromImperialUnit(PoundUnit unit, double value) {
+            return (value * unit.Coefficient) * 28.3495 / this.Coefficient;
+        }
+    }
+}
---
+++ WPF/SampleWeightUnitConverter/Model/PoundUnit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWeightUnitConverter {
+    internal class PoundUnit : WeightUnit {
+        private static readonly List<PoundUnit> units = new List<PoundUnit> {
+            
+            new PoundUnit { Name = "oz", Coefficient = 1 },          
+            new PoundUnit { Name = "lb", Coefficient = 16 },       
+            new PoundUnit { Name = "st", Coefficient = 224 },       
+            
+        };
+
+        public static ICollection<PoundUnit> Units { get { return units; } }
+
+        public double FromMetricUnit(GramUnit unit, double value) {
+            return (value * unit.Coefficient) / 28.3495 / this.Coefficient;
+        }
+    }
+}
