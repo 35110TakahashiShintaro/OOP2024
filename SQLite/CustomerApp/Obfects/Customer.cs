366818679df223c105ddbc9b9e059cf0fe6e7129
TakahashiShintaro <166583699+35110TakahashiShintaro@users.noreply.github.com>
2024-11-21 09:29:03 +0900

    画像を保存、削除、アップデート、ListViewに表示 完成

--- SQLite/CustomerApp/Obfects/Customer.cs
+++ SQLite/CustomerApp/Obfects/Customer.cs
@@ -1,9 +1,11 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace CustomerApp.Objects {
     public class Customer {
@@ -21,6 +23,21 @@ namespace CustomerApp.Objects {
         public string Address { get; set; }
 
         public byte[] ImageData { get; set; }
+
+        public BitmapImage ImageSource {
+            get {
+                if (ImageData == null || ImageData.Length == 0) return null;
+
+                using (var stream = new MemoryStream(ImageData)) {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+                    return image;
+                }
+            }
+        }
 
         public override string ToString() {
             return $"{Id}  {Name}  {Phone} {Address}";
